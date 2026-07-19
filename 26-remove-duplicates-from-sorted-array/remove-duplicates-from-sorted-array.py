adc23cf965a89846fb27d3f03ae85092d4d5e278
Sannitha Telliboina <sannithatelliboina@gmail.com>
2026-07-19 16:01:30 +0530

    Time: 5 ms (15.59%) | Memory: 20.5 MB (42.18%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        writepos = 0
+        for readpos in range(1,len(nums)):
+            if nums[readpos] != nums[writepos]:
+                writepos += 1
+                nums[writepos] = nums[readpos]
+        return writepos + 1
